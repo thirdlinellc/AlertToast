4b1e4e9629a461531e5ad2111f4f4d684adeef5d
henrypratt <henrypratt9@gmail.com>
2024-02-17 09:35:22 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "AlertToast",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v11)
+        .iOS(.v16),
+        .macOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
